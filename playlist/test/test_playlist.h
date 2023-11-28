d5c2a4d2cdad263bac53287ced88aab7d2c8454c
Israel Marinho <ilm.eletrica@gmail.com>
2023-11-27 21:41:33 -0400

    Add GTest to the project

--- playlist/test/test_playlist.h
+++ playlist/test/test_playlist.h
@@ -1,19 +1,50 @@
 #include <gtest/gtest.h>
+#include <memory>
 
 #include "playlist.h"
 
-TEST(Playlist,  Creation)
+class PlaylistTest : public testing::Test 
 {
-     std::list<playing_with_cpp::song::Song> songs
-     {
-        {"God's Plan",        "Drake",                     5},
-        {"Never Be The Same", "Camila Cabello",            5},
-        {"Pray For Me",       "The Weekend and K. Lamar",  4},
-        {"The Middle",        "Zedd, Maren Morris & Grey", 5},
-        {"Wait",              "Maroone 5",                 4},
-        {"Whatever It Takes", "Imagine Dragons",           3}
-    };
-    playing_with_cpp::playlist::Playlist playlist(songs);
+protected:    
+    std::unique_ptr<playing_with_cpp::playlist::Playlist> playlistPtr;
 
-    ASSERT_EQ(playlist.GetCurrentSong().GetName(), "God's Plan");
+    void SetUp() override
+    {
+        std::list<playing_with_cpp::song::Song> songs
+        {
+            {"God's Plan",        "Drake",                     5},
+            {"Never Be The Same", "Camila Cabello",            5},
+            {"Pray For Me",       "The Weekend and K. Lamar",  4},
+            {"The Middle",        "Zedd, Maren Morris & Grey", 5},
+            {"Wait",              "Maroone 5",                 4},
+            {"Whatever It Takes", "Imagine Dragons",           3}
+        };
+        playlistPtr = std::make_unique<playing_with_cpp::playlist::Playlist>(songs);
+    }
+
+};
+
+TEST_F(PlaylistTest, Creation)
+{
+    ASSERT_EQ(playlistPtr->GetCurrentSong().GetName(), "God's Plan");
+}
+
+TEST_F(PlaylistTest, NextSong)
+{
+    playlistPtr->PlayNext();
+    ASSERT_EQ(playlistPtr->GetCurrentSong().GetName(), "Never Be The Same");
+}
+
+TEST_F(PlaylistTest, PreviousSong)
+{
+    playlistPtr->PlayPrevious();
+    ASSERT_EQ(playlistPtr->GetCurrentSong().GetName(), "Whatever It Takes");
+}
+
+TEST_F(PlaylistTest, InsertSong)
+{
+    playing_with_cpp::song::Song song("Indestructible", "Welshly Arms", 5);
+    playlistPtr->InsertSong(song);
+
+    ASSERT_EQ(playlistPtr->GetCurrentSong().GetName(), "Indestructible");
 }
